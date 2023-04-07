a5b779729562b730942251cd5862d91506f6adcb
haydenwelton <112789689+haydenwelton@users.noreply.github.com>
2023-04-06 22:28:37 -0600

    Messed up dunno why

--- public/myburger.js
+++ public/myburger.js
@@ -1,3 +1,30 @@
+
+const container = document.getElementById("card-container");
+
+function createCards(burgers) {
+  burgers.forEach(item => {
+    //Create card
+    const card= document.createElement("div");
+    card.classList.add("card");
+
+    //Create name element
+    const name = document.createElement("h2");
+    name.innerText = item.name;
+    card.appendChild(name);
+
+    //Create details element
+    const details = document.createElement("p");
+    details.innerText = item.description;
+    card.appendChild(details);
+
+    container.appendChild(card);
+  })
+};
+
+
+
+
+
 const getMyBurgers = async () => {
   try {
     const response = await fetch("/api/burgers/myburgers", {
@@ -9,6 +36,7 @@ const getMyBurgers = async () => {
 
     console.log("data", response);
     console.log(await response.json());
+    createCards(await response.json());
   } catch (error) {
     console.error(error);
     alert("Unable to add burger");
